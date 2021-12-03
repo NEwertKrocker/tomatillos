97eb1a5581915b5d66d1bacc6ac6045ea39eede8
jose Tovar <jose.tovar102397@gmail.com>
2021-12-02 21:12:10 -0700

    Add jsx elements to display movie posters

--- src/MoviePoster.js
+++ src/MoviePoster.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './css/MoviePoster.css'
 
 const MoviePoster = (props) => {
   console.log('props>>>', props);
   return (
-    <article>
-      <p>rating {props.rating}</p>
+    <article className='movie-poster'>
+      <img src={props.posterPath} alt={props.title} />
+      <section className='movie-poster-details'>
+        <h2>{props.title}</h2>
+        <p>Rating: {props.rating}</p>
+      </section>
     </article>
   )
 }
