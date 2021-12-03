7e9e6ef1491964be0f5ee6a49296dd4bd4ededae
jose Tovar <jose.tovar102397@gmail.com>
2021-12-02 19:03:29 -0700

    Create movies component with functionality to render movies poster

---
+++ src/Movies.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import MoviePoster from './MoviePoster';
+
+const Movies = ({ movies }) => {
+  // console.log(movies.movies);
+  const moviePosters = movies.movies.map(movie => {
+    // console.log("<in Movie Posters>>", movie)
+    return (
+      <MoviePoster 
+        // movie={movie}
+        id={movie.id}
+        posterPath={movie['poster_path']}
+        backdropPath={movie['backdrop_path']}
+        title={movie.title}
+        rating={movie['average_rating']}
+        releaseDate={movie['release_date']}
+        key={movie.id}
+      />
+    )
+  });
+
+  return (
+    <section>
+      {moviePosters}
+    </section>
+  )
+}
+
+export default Movies;
