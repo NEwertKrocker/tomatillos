56ca9e40dc65e4c9765b531948eccb1f27474639
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-12-03 11:58:30 -0500

    Add dummy movie details to App.js

--- src/MoviePoster.js
+++ src/MoviePoster.js
@@ -5,11 +5,11 @@ const MoviePoster = (props) => {
   console.log('props>>>', props);
   const rating = Math.floor(props.rating * 10) / 10;
   return (
-    <article className='movie-poster'>
+    <article className='movie-poster' onClick={(event) => displayDetails(id)}>
       <img src={props.posterPath} alt={props.title} />
       <section className='movie-poster-details'>
         <h2>{props.title}</h2>
-        <p>Rating: {rating}</p>
+        <p>{rating} ⭐️</p>
       </section>
     </article>
   )
