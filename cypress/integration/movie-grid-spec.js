d1de74dcbeda76d3793091865daf4093e5326c51
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-12-06 16:12:18 -0500

    Add test for navigating to movie details view

--- cypress/integration/movie-grid-spec.js
+++ cypress/integration/movie-grid-spec.js
@@ -10,4 +10,10 @@ describe('Default movie grid view', () => {
   it('should display an array of movie posters', () => {
     cy.get('.movie-poster').should('have.length', 40)
   })
+  it('should be able to navigate to movie details view', () => {
+    cy.get('.movie-poster')
+      .first()
+      .click();
+    cy.get('.details-poster');
+  })
 })
