1f1f1b85cd689ab70ba49e36d36058fdd3fb9536
Nate Ewert-Krocker <n.ewertkrocker@gmail.com>
2021-12-07 16:04:40 -0500

    Add tomatoes image for use in nav bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Form from './Form'
 import './css/NavBar.css'
+import './assets/tomatoes.jpeg'
+
+const backgroundStyle = {
+  backgroundImage:
+  `url('./assets/tomatoes.jpeg')`,
+}
 
 const NavBar = () => {
   return (
-    <header className='nav-bar'>
+    <header className='nav-bar' style={backgroundStyle}>
       <h1>Rancid Tomatillos</h1>
       <Form />
     </header>
