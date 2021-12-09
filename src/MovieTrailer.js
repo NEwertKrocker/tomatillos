97e392dd839a85b9eeecfed3856550a811a22610
jose Tovar <jose.tovar102397@gmail.com>
2021-12-08 21:54:27 -0700

    Add Movie trailer class based component

---
+++ src/MovieTrailer.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import { getMovieVideos } from "./apiCalls";
+
+class MovieTrailer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movieVideo: {}
+    }
+  }
+
+  componentDidMount() {
+    getMovieVideos(this.props.id)
+      .then(data => {
+        const video = data.videos.find(video => video.type.includes('Trailer'));
+        console.log(video)
+        this.setState({ movieVideo: video})
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <iframe
+          title='Movie Trailer'
+          src={`https://www.youtube.com/embed/${this.state.movieVideo.key}?autoplay=1`}
+          frameBorder=''
+          allow='autoplay; encrypted-media'
+          allowFullScreen
+          width={960}
+          height={544}
+          objectFit= 'cover'
+        >
+        </iframe>
+      </>
+    )
+  }
+}
+
+export default MovieTrailer;
