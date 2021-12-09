5aa5b34b3b4e53c60768c118983c2d16bc7a1eff
jose Tovar <jose.tovar102397@gmail.com>
2021-12-08 21:17:43 -0700

    Refactor movie details component to be a class based component

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import './css/MovieDetails.css'
+import React, { Component } from 'react';
+import './css/MovieDetails.css';
+import { getSingleMovie, getMovieVideos } from './apiCalls';
 import backIcon from './assets/back-arrow.svg'
 import { Link } from 'react-router-dom';
-import Movies from './Movies'
+// import Movies from './Movies'
 
-const MovieDetails = (props) => {
-  const movieTrailer = props.movieVideos.find(video => video.type.includes('Trailer'))
-  const details = props.details;
-  const rating = Math.floor(details.average_rating * 10) / 10;
-  const yearRelease = new Date(details.release_date).getFullYear()
-  const backgroundStyle = {
-    backgroundImage:
-    `linear-gradient(to right, #1C1D1E, 50%, transparent),
-     url(${props.details['backdrop_path']})`,
-     backgroundRepeat: 'no-repeat',
-     width: '100%',
-     height: '100%',
-     backgroundPosition: 'center',
-     backgroundSize: 'cover'
+class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movieDetails: {}
+    }
   }
 
   return (
