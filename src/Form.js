280925668d6772b1623b0bfe8c6eb7121095a034
jose Tovar <jose.tovar102397@gmail.com>
2021-12-11 11:49:59 -0700

    Refactor form component to be a functional component

--- src/Form.js
+++ src/Form.js
@@ -1,32 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './css/Form.css';
 
-class Form extends Component {
-  constructor() {
-    super();
-    this.state = {
-      searchBarValue: ''
-    }
-  }
-
-  handleChange = event => {
-    this.setState({ searchBarValue: event.target.value });
-    this.props.searchMovies(this.state.searchBarValue);
-  }
-
-  render() {
-    return (
-      <form>
-        <input 
-          type='text'
-          name='searcBarValue'
-          placeholder='Search Movie...'
-          value={this.state.searchBarValue}
-          onChange={event => this.handleChange(event)}
-        />
-      </form>
-    )
-  }
+const Form  = ({ searchMovies }) => {
+  return (
+    <form>
+      <input
+        type='text'
+        name='searcBarValue'
+        placeholder='Search Movie...'
+        onChange={event => searchMovies(event.target.value)}
+      />
+    </form>
+  )
 }
 
 export default Form;
