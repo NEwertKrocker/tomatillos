3923c48f251fc6d9d766b51c145c6b29efef48cc
jose Tovar <jose.tovar102397@gmail.com>
2021-12-11 11:51:48 -0700

    Add filtered movie posters to display based on user input

--- src/Movies.js
+++ src/Movies.js
@@ -3,8 +3,7 @@ import MoviePoster from './MoviePoster';
 import './css/Movies.css'
 import { Link } from 'react-router-dom';
 
-const Movies = ({ movies }) => {
-
+const Movies = ({ movies, filteredMovies }) => {
   const moviePosters = movies.map(movie => {
     return (
       <Link to={`${movie.id}`} key={movie.id}>
@@ -19,13 +18,27 @@ const Movies = ({ movies }) => {
         />
       </Link>
     )
-
-    
   });
+
+  const filteredMoviePosters = filteredMovies.map(movie => {
+    return (
+      <Link to={`${movie.id}`} key={movie.id}>
+        <MoviePoster
+          id={movie.id}
+          posterPath={movie['poster_path']}
+          backdropPath={movie['backdrop_path']}
+          title={movie.title}
+          rating={movie['average_rating']}
+          releaseDate={movie['release_date']}
+          key={movie.id}
+        />
+      </Link>
+    )
+  })
 
   return (
     <section className='movies-container'>
-      {moviePosters}
+      {!filteredMovies.length ? moviePosters : filteredMoviePosters}
     </section>
   )
 }
