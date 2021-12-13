ad69a0c5e5a373adec3b0f916fbf6c7bf70f7815
jose Tovar <jose.tovar102397@gmail.com>
2021-12-13 10:58:27 -0700

    Add paragraph to display movie runtime

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -50,10 +50,11 @@ class MovieDetails extends Component {
         </Link>
         <article className="movie-details-container">
           <section className='movie-details'>
-            <div className='basic-info'>
               <h2 className='movie-title'> {movie.title} </h2>
+            <div className='basic-info'>
               <p className='stats'> {this.getReleaseYear(movie)} </p>
               <p className='stats'> {`⭐️ ${this.getRating(movie)}`} </p>
+              <p className='stats'>{`${movie.runtime} mins`}</p>
             </div>
             <p> {movie.tagline} </p>
             <p> {movie.overview} </p>
